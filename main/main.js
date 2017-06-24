089dc12f71e53767018bf4e93cb39d14ae168d60
voiweb <1172673619@qq.com>
2017-06-24 22:33:48 +0800

    my commit

--- main/main.js
+++ main/main.js
@@ -1,3 +1,11 @@
+
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var arr = email.split('@');
+  var str = arr[1];
+  if (str == suffixes)
+		return true;
+  else
+		return false;
+ 
 };
